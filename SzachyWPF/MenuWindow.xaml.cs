660d1077999aea507b803e9a1b28d31bc6877681
Justyna Zbiegien <justyna_zbiegien@wp.pl>
2018-12-30 23:43:13 +0100

    Dodalam nowe okienko z menu. Button "Nowa gra z komputerem" otwiera MainWindow (jak poprzednio). Pozostale buttony pokazują MessageBox "Nie zaimplementowano".

---
+++ SzachyWPF/MenuWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SzachyWPF
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Window1.xaml
+    /// </summary>
+    public partial class MenuWindow : Window
+    {
+        public MenuWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void nowaGraAIButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Closing += ShowMenu;
+            mainWindow.Show();
+            this.Close();
+        }
+
+        public static void ShowMenu(object sender, CancelEventArgs e)
+        {
+            MenuWindow menu = new MenuWindow();
+            menu.Show();
+        }
+
+        private void nowaGraCzlowiekButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Nie zaimplementowano");
+        }
+
+        private void wczytajGreButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Nie zaimplementowano");
+        }
+
+        private void zapiszGreButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Nie zaimplementowano");
+        }
+
+        private void tablicaWynikowButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Nie zaimplementowano");
+        }
+    }
+}
