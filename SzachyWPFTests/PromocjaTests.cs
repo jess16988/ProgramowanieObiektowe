f3eddf36a8ab2401605708cf6d5f6e03c59ad235
Pimpek <Pimpek@DESKTOP-URAG47E>
2019-01-15 11:55:11 +0100

    Wprowadziłam testy jednostkowe

---
+++ SzachyWPFTests/PromocjaTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SzachyWPF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzachyWPF.Tests
+{
+    [TestClass()]
+    public class PromocjaTests
+    {
+        [TestMethod()]
+        public void SprawdzTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ SzachyWPFTests/RuchAITests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SzachyWPF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzachyWPF.Tests
+{
+    [TestClass()]
+    public class RuchAITests
+    {
+        [TestMethod()]
+        public void RuchAITest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CompareToTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
